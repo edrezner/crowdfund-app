ff757395f60f80738d6686afee2bb163d5a0d578
Seven <edrezner7@gmail.com>
2025-07-25 18:57:57 -0400

    Corrected model route in userRoutes, created /login route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User } = require("../../models/User");
+const { User } = require("../../models");
 
 router.post("/", async (req, res) => {
   try {
@@ -10,6 +10,35 @@ router.post("/", async (req, res) => {
       req.session.logged_in = true;
 
       res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      return res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again." });
+    }
+
+    const validPassword = userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      return res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again." });
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json({ user: userData, message: "Login successful!" });
     });
   } catch (err) {
     res.status(400).json(err);
