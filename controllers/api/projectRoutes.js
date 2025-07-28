647951726e21ee7c5c0906f4d291919dc4dc2411
Seven <edrezner7@gmail.com>
2025-07-28 15:53:11 -0400

    Added a project delete route

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -14,3 +14,31 @@ router.post("/", withAuth, async (req, res) => {
     res.status(400).json(err);
   }
 });
+
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const projectForDeletion = await Project.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!projectForDeletion) {
+      return res.status(404).json({ message: "Project not found." });
+    }
+
+    if (projectForDeletion.user_id !== req.session.user_id) {
+      return res.status(403).json({
+        message: "You do not have permission to delete this project.",
+      });
+    }
+
+    await projectForDeletion.destroy();
+
+    res.status(200).json(projectForDeletion);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
