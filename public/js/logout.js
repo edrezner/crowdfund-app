106133aacf2d34eb7a6e5ce33efa2423ed3e2fb7
Seven <edrezner7@gmail.com>
2025-08-01 20:27:22 -0400

    Added a logout.js file and profile.handlebars file

---
+++ public/js/logout.js
@@ -0,0 +1,18 @@
+const logoutButton = document.querySelector("#logout");
+
+const logoutHandler = async () => {
+  const response = await fetch("/api/users/logout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/login");
+  } else {
+    alert(response.statusText || "Failed to log out. Please try again.");
+  }
+
+  if (logoutButton) {
+    logoutButton.addEventListener("click", logoutButton);
+  }
+};
